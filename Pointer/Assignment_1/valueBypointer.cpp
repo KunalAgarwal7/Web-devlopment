38f3c3d315df499be8dfb2f6765c37650ea8b3c7
Kunal Agarwal <mrkunalagarwal33@gmail.com>
2025-07-25 08:36:31 +0530

    Pointer

--- Pointer/Assignment_1/valueBypointer.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int num = 10;
-    int *add = &num;
-    cout << "Address is: " << add << endl;
-    cout << "Value using pointer:" << *add;
-}
